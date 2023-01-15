fc9a7dce730a9a020af9c54fb75dfdd3e88f404f
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-01-15 05:52:22 +0100

    Bionic text base

---
+++ simple_bionic/src/components/BionicText.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useSlots } from 'vue'
+
+const bionicizeText = (text: String) => {
+    const words = text.split(" ").map((word) => {
+        const middle = Math.floor(word.length / 2);
+        const first_half = word.slice(0, middle);
+        const second_half = word.slice(middle);
+        return `<b>${first_half}</b>${second_half}`
+    })
+    return words.join(" ")
+};
+
+const slots = useSlots()
+const text = slots.default!().map((elem) => elem.children).join(" ")
+const bionicText = bionicizeText(text)
+
+</script>
+
+<template>
+<span v-html="bionicText" class="font-normal"></span>
+</template>
--- simple_bionic/src/views/user/HomeView.vue
+++ simple_bionic/src/views/user/HomeView.vue
@@ -1,11 +1,45 @@
 <script setup lang="ts">
 import MainButton from "../../components/MainButton.vue";
+import BionicText from "../../components/BionicText.vue";
 </script>
 
 <template>
   <main>
     <div class="flex justify-center items-center h-screen">
         <div class="flex flex-col space-y-12">
+          <BionicText>
+            These are less formulations of a position than fragmentary and largely
+programmatic aspects of Zarathustra’s self-diagnosis and the cure he at
+least aspires to. Many philosophical questions arise inevitably.Whatwould
+be amiss, lost, wrong in a life not fully or not at all “led” by a subject?
+How could this aspiration towards something believed to be higher or
+more worthy than what one is or has now be directed, if all the old language
+of external or objective forms of normative authority is now impossible?
+On what grounds can one say that a desire to cultivate a different sort of
+self, to overcome oneself, is really in the service of a “higher” self? Higher
+in what sense? What could be said to be responsible for (relied on for)
+securing this obedience, for helping to ward off skepticism when it arises?
+Under what conditions can such commitments and projects be said to
+lose their grip on a subject, fail, or die?
+In general Zarathustra does not fully accept the burden of these questions as ones he must assume. For one thing he clearly does not believe
+that the inspiration for such an attempt at self-direction and something
+like “becoming better at becoming who one is” can be provided by an
+argument or a revelation or a command. One would already have had to
+measure oneself and one’s worth against “arguments” or “revelation” or
+“authoritative commands” for such different calls to be effective and it
+is to that prior, deepest level of commitment that Zarathustra, however
+indirectly and figuratively, is directing his rhetoric. And given the great
+indeterminateness of his approach, he is clearly much more interested
+in the qualitative characteristics of such commitments than with their
+content. The quality he is most interested in turns out to be extremely
+complex: on the one hand, “whole-heartedness” and an absorbed or passionate “identification” with one’s higher ideal; on the other hand, a
+paradoxical capacity to “let go” of such commitments and pursue other
+ideals when the originals (somehow) cease to serve self-overcoming and
+self-transcendence, when they lead to complacency and contentment.
+However, to come to by far the most complicated issue introduced
+by Zarathustra’s speeches, he clearly also thinks that such qualitative
+considerations – the chief topic of the book, the qualitative dimensions
+          </BionicText>
           <MainButton class="bg-coffee" to="/about">Zadania</MainButton>
           <MainButton class="bg-coffee-milk" to="/about">Wgraj tekst</MainButton>
           <MainButton class="bg-dark-gray" to="#">Wyjdź</MainButton>
